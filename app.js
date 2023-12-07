478b3c82fdbdf1f4df671a4a4158031fb5e6c7ff
Kaveh HN <kavehhn174@gmail.com>
2023-12-08 01:10:52 +0330

    ADD: Fitness Calculator ADD: Sample Creator

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const csv = require('csvtojson');
 const csvFilePath = './data.csv';
-const random = require('random');
 
 // Constraints :
 const MaxWeight = 220;
@@ -8,8 +7,39 @@ const MaxSize = 2;
 let Items = [];
 const NumberOfGenerations = 300;
 const NumberOfSamples = 200;
+const Samples = [];
+const TestItems = [
+    0,
+    1,
+    1,
+    0,
+    0,
+    0,
+    1,
+    1,
+    0,
+    0,
+    1,
+    0,
+    0,
+    1,
+    1,
+    0,
+    1,
+    0,
+    1,
+    0,
+    0,
+    1,
+    0,
+    1,
+    1,
+    0,
+    0,
+    0,
+    0,
+]
 
-main();
 
 
 // Generate Random Array For All The Items In The Vault
@@ -18,11 +48,48 @@ const getRandomInt = max => Math.floor(Math.random() * max);
 // Loads CSV Of Data Into Item Array as JSON
 const loadJson = async () => Items = await csv().fromFile(csvFilePath);
 
+// It Checks If A List Of Selected Items Is Acceptable Or Not
+function calculateFitness(UsedItemsArray) {
+    let weight = 0;
+    let size = 0;
+    let value = 0;
+    for (let i = 0; i < UsedItemsArray.length; i++) {
+        if (UsedItemsArray[i] === 1) {
+            weight += parseInt(Items[i].weight);
+            size += parseFloat(Items[i].size);
+            size = parseFloat(size.toFixed(2))
+            value += parseInt(Items[i].value);
+        }
+        console.log(weight,'/', size, '/', value)
+    }
+    if (weight > MaxWeight) {
+        return 0;
+    }
+    if (size > MaxSize) {
+        return 0;
+    }
+    return value;
+}
 
+
+// Create Population
+
+function createPopulation() {
+    for (let i = 0; i < NumberOfSamples; i++) {
+        const tempArray = [];
+        for (let j = 0; j < Items.length; j++) {
+            tempArray.push(getRandomInt(2))
+        }
+        Samples.push(tempArray);
+    }
+}
 // Main Function To Run The Program
 async function main() {
     await loadJson()
-    console.log(Items)
+    // calculateFitness(TestItems)
 
-
+    createPopulation();
+    console.log(Samples)
 }
+
+main();
